4225785a4d649bd7184f17b8aa80f3bd98108e22
Irfaan <irfaank778@gmail.com>
2024-07-24 12:29:18 +0530

    added api for fetching pdf

---
+++ app/api/uploadnotes/route.ts
@@ -0,0 +1,88 @@
+// pages/api/upload.js
+import connect from "@/lib/db";
+import { GridFSBucket, ObjectId } from "mongodb";
+import { NextResponse } from "next/server";
+import { Readable } from "stream";
+
+const dataURLtoBuffer = (dataURL: string) => {
+  const parts = dataURL.split(";base64,");
+  if (parts.length !== 2) {
+    throw new Error("Invalid Data URL");
+  }
+
+  return Buffer.from(parts[1], "base64");
+};
+
+export const POST = async (req: any) => {
+  try {
+    const {
+      dataURL,
+      fileName,
+      course,
+      subject,
+      description,
+      year,
+      semester,
+      userID,
+    } = await req.json();
+
+    const client = await connect();
+    const database = client.db("easynotes");
+    const bucket = new GridFSBucket(database, { bucketName: "uploads" });
+
+    const buffer = dataURLtoBuffer(dataURL);
+
+    const readableStream = new Readable();
+    readableStream._read = () => {}; // _read is required but you can noop it
+    readableStream.push(buffer);
+    readableStream.push(null);
+
+    const uploadStream = bucket.openUploadStream(fileName, {
+      metadata: {
+        course,
+        subject,
+        description,
+        year,
+        semester,
+        userID: new ObjectId(userID),
+      },
+    });
+
+    readableStream.pipe(uploadStream);
+
+    await new Promise<void>((resolve, reject) => {
+      uploadStream.on("finish", async () => {
+        try {
+          await database.collection("uploads").insertOne({
+            course,
+            subject,
+            year,
+            description,
+            semester,
+            fileID: uploadStream.id,
+            userID: new ObjectId(userID),
+          });
+          resolve();
+        } catch (err) {
+          reject(err);
+        }
+      });
+
+      uploadStream.on("error", (err) => {
+        reject(err);
+      });
+    });
+
+    client.close();
+    return NextResponse.json(
+      { message: "File uploaded successfully", fileId: uploadStream.id },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error("Failed to process Data URL:", error);
+    return NextResponse.json(
+      { error: "Failed to process Data URL" },
+      { status: 500 }
+    );
+  }
+};
